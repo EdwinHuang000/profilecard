6e299547e92975804197bf66d48f60b95e75d7df
EdwinHuang000 <103179293+EdwinHuang000@users.noreply.github.com>
2023-12-16 14:03:34 +1100

    new update with the fetching data

--- profilecard/src/App.js
+++ profilecard/src/App.js
@@ -1,21 +1,61 @@
 import "./App.css"
 
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA"
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D"
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF"
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33"
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB"
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00"
+  }
+]
+
 function App() {
+  const skillDatas = skills
+  const numSkills = skills.length
+
   function SkillList() {
     return (
       <div className="skill-list">
-        <Skill skill="React" emoji="💪" color="blue" />
-        <Skill skill="HTML+CSS" emoji="💪" color="orange" />
-        <Skill skill="JavaScript" emoji="💪" color="yellow" />
-        <Skill skill="Svelte" emoji="👶" color="orangered" />
+        {skillDatas.map(skillData => (
+          <Skill skill={skillData.skill} color={skillData.color} level={skillData.level} />
+        ))}
       </div>
     )
   }
-  function Skill(props) {
+  function Skill({ skill, color, level }) {
     return (
-      <div className="skill" style={{ backgroundColor: props.color }}>
-        <span>{props.skill}</span>
-        <span>{props.emoji}</span>
+      <div className="skill" style={{ backgroundColor: color }}>
+        <span>{skill}</span>
+        <span>
+          {" "}
+          {level === "beginner" && "👶"}
+          {level === "intermediate" && "👍"}
+          {level === "advanced" && "💪"}
+        </span>
       </div>
     )
   }
